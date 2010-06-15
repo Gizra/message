211feb1f81ac431c75ce0cbc17a1b2339f2217d8
Amitaibu <amitaibu@gizra.com>
2010-06-15 15:04:58 +0300

    Remove class file.

--- plugins/export_ui/message_ctools_export_ui.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-// $Id: message_ctools_export_ui.class.php,v 1.1 2010/06/07 10:54:11 amitaibu Exp $
-
-/**
- * Base class for export UI.
- */
-class message_ctools_export_ui extends ctools_export_ui {
-}
