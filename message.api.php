a7443cae478a86d684f1974f0a74c9ee952098bb
Amitaibu <amitai@gizra.com>
2010-07-27 23:49:24 +0300

    Better exmaple in API file.

--- message.api.php
+++ message.api.php
@@ -15,25 +15,14 @@
 /**
  * Alter the message instances
  *
- * @param $messages
+ * @param $message_instances
  *   The loaded message instances passed by reference.
  */
-function hook_message_instances_alter(&$messages) {
-  // Unfiy similar message instances to a single one:
-  // - The message name is "create_new_content"
-  // - The message itself is "created <a href="@link">@title</a>."
-  // - The message itself is "created @content"
-  // - The callback of the @content argument returns <a href="foo">bar</a>
-  // - We will replace the callback with a value that unifies the messages
-  $join = array();
-  foreach($messages as $key => $value) {
-    if ($value->name == 'create_new_content') {
-      $join[$key] = $value;
-    }
+function hook_message_instances_alter(&$message_instances) {
+  if ($message_instances->name == 'foo') {
+    // Hide the message instance.
+    $message_instances->hide = TRUE;
   }
-
-
-
 
 }
 
