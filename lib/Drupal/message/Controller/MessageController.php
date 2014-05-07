e568244683c0f51c63f9c7be4401773d1be98691
RoySegall <roy.segall@gizra.com>
2014-05-07 10:08:26 +0300

    Start working on the sandbox page.

---
+++ lib/Drupal/message/Controller/MessageController.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\message\Controller\MessageController.
+ */
+
+namespace Drupal\message\Controller;
+
+/**
+ * Returns responses for System Info routes.
+ */
+class MessageController {
+
+  /**
+   * This is the sandbox page. Here we will all the thing we need to do during
+   * the message develoment to D8.
+   */
+  static public function page() {
+  }
+}
