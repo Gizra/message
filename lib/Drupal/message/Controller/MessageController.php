ed3c2820eaa8ba447ed1dd7320f59ede792b6164
Roy Segall <roy.segall@gizra.com>
2014-05-09 15:53:03 +0300

    Improve the sandbox.

--- lib/Drupal/message/Controller/MessageController.php
+++ lib/Drupal/message/Controller/MessageController.php
@@ -19,19 +19,68 @@ class MessageController extends ControllerBase {
    * the message develoment to D8.
    */
   public function page() {
-    // Create type.
-    if (!$type = entity_load('message_type', 'testing')) {
-      $type = entity_load('message_type', array('type' => 'testing', 'label' => 'Testing'));
-      $type->save();
+    $types = array(
+      array(
+        'type' => 'testing',
+        'label' => 'Testing',
+        'text' => array(
+          'this is testing. created at @time',
+        ),
+      ),
+      array(
+        'type' => 'foo',
+        'label' => 'Foo',
+        'text' => array(
+          'this is testing. created by @name',
+        ),
+      ),
+      array(
+        'type' => 'bar',
+        'label' => 'Bar',
+        'text' => array(
+          'this is testing. created by @name',
+        ),
+      ),
+      array(
+        'type' => 'roy',
+        'label' => 'Roy',
+        'text' => array(
+          'this is testing. created by @name',
+        ),
+      ),
+      array(
+        'type' => 'segall',
+        'label' => 'Segall',
+        'text' => array(
+          'this is testing. created by @name',
+        ),
+      ),
+    );
+
+    foreach ($types as $type) {
+      // Create type.
+      if (!$message_type = entity_load('message_type', $type['type'])) {
+        $message_type = entity_create('message_type', $type);
+        $message_type->save();
+      }
+    }
+
+    $message_types = entity_load_multiple('message_type');
+
+    $output[] = "<h2>" . t('Message types') . "</h2>";
+
+    foreach ($message_types as $message_type) {
+      $output[] = $message_type->id() . ' ' . $message_type->label() . ' ' .  $message_type->getText();
     }
 
     entity_create('message', array('type' => 'testing', 'arguments' => array('time' => time())))->save();
     $messages = entity_load_multiple('message');
 
+    $output[] = "<h2>" . t('Message IDs') . "</h2>";
     foreach ($messages as $message) {
-      $id[] = $message->id();
+      $output[] = $message->id();
     }
 
-    return implode("<br />", $id);
+    return implode("<br />", $output);
   }
 }
