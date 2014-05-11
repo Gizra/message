f342f26910acd89294d774fb95de5d1ad7308ced
RoySegall <roy.segall@gizra.com>
2014-05-11 09:44:01 +0300

    Adding two API functions.

--- lib/Drupal/message/Controller/MessageController.php
+++ lib/Drupal/message/Controller/MessageController.php
@@ -17,6 +17,19 @@ use Drupal\message\Entity\Message;
 class MessageController extends ControllerBase {
 
   /**
+   * Create a message type.
+   *
+   * @param String $type
+   *  The message type.
+   *
+   * @return MessageType
+   *  A message type object ready to be save.
+   */
+  public static function MessageTypeCreate($type) {
+    return entity_create('message_type', array('type' => $type));
+  }
+
+  /**
    * Loading a message type.
    *
    * @param String $type
@@ -39,6 +52,19 @@ class MessageController extends ControllerBase {
    */
   public static function MessageTypeLoadMultiple(array $types) {
     return entity_load_multiple('message_type', $types);
+  }
+
+  /**
+   * Create a message.
+   *
+   * @param Array $values
+   *  The message type.
+   *
+   * @return Message
+   *  A message type object ready to be save.
+   */
+  public static function MessageCreate($values) {
+    return entity_create('message', $values);
   }
 
   /**
