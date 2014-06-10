0ed6e2445cb6b760a52e8d3cc4025c1099256f2c
RoySegall <roy.segall@gizra.com>
2014-06-10 10:13:27 +0300

    Adding documentation why we altering the configEntityMapper.

--- src/MessageConfigEntityMapper.php
+++ src/MessageConfigEntityMapper.php
@@ -11,9 +11,35 @@ use Drupal\config_translation\ConfigEntityMapper;
 use Symfony\Component\Routing\Route;
 
 /**
- * Configuration mapper for message config entity. We need to take over the
- * config entity mapper for the message type entity due to the multiple text
- * element.
+ * Configuration mapper for message config entity.
+ *
+ * Why do we need to override the original mapper? As i mentioned in the save
+ * method of the entity type message the message type in Drupal 7 was a
+ * fieldable entity. Since the message type is a config entity we can't add the
+ * field and translate it easily. We solved it by defining a sequence field and
+ * managing the partial by our self. The field is managed by the next format:
+ *
+ * language => array(
+ *  'en' => array(
+ *    0 => 'First prtial',
+ *    1 => 'Second partial',
+ *  ),
+ *  'fr' => array(
+ *    0 => 'première partie',
+ *    1 => 'deuxième partie',
+ *  ),
+ * );
+ *
+ * When facing the translation we will need to extend the edit/delete/insert
+ * forms and add the multiple text field element. The manging of the field will
+ * occur in the submission of the forms(edit/delete/insert) and the user won't
+ * notice any different.
+ *
+ * This will also affect the MessageType::getText() method will need to check
+ * the current site language all pull the text in the current language.
+ *
+ * @see MessageType::save()
+ * @see MessageType::getText()
  */
 class MessageConfigEntityMapper extends ConfigEntityMapper {
 
