0daad649290f3422edf1f1f9e7b0c154dd665aeb
Roy Segall <roy.segall@gizra.com>
2014-06-13 17:25:48 +0300

    Done working on the config form.

--- src/Form/MessageSettingsForm.php
+++ src/Form/MessageSettingsForm.php
@@ -23,60 +23,72 @@ class MessageSettingsForm extends ConfigFormBase {
   }
 
   /**
+   * Holds the name of the keys we holds in the variable.
+   */
+  public function defaultKeys() {
+    return array('purge_enable', 'purge_quota', 'purge_days', 'delete_on_entity_delete');
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, array &$form_state) {
     $config = $this->config('message.message');
-
-//      '#default_value' => $config->get('path.temporary'),
 
     $form['purge'] = array(
       '#type' => 'fieldset',
       '#title' => t('Purge settings'),
     );
 
-    $form['purge']['message_purge_enable'] = array(
+    $form['purge']['purge_enable'] = array(
       '#type' => 'checkbox',
       '#title' => t('Purge messages'),
       '#description' => t('When enabled, old messages will be deleted.'),
-//      '#default_value' => variable_get('message_purge_enable', FALSE),
+      '#default_value' => $config->get('purge_enable'),
     );
 
     $states = array(
       'visible' => array(
-        ':input[name="message_purge_enable"]' => array('checked' => TRUE),
+        ':input[name="purge_enable"]' => array('checked' => TRUE),
       ),
     );
 
-    $form['purge']['message_purge_quota'] = array(
+    $form['purge']['purge_quota'] = array(
       '#type' => 'textfield',
       '#title' => t('Messages quota'),
       '#description' => t('Maximal (approximate) amount of messages.'),
-//      '#default_value' => variable_get('message_purge_quota', NULL),
-      '#element_validate' => array('element_validate_integer_positive'),
+      '#default_value' => $config->get('purge_quota'),
+//      '#element_validate' => array('element_validate_integer_positive'),
       '#states' => $states,
     );
 
-    $form['purge']['message_purge_days'] = array(
+    $form['purge']['purge_days'] = array(
       '#type' => 'textfield',
       '#title' => t('Purge messages older than'),
       '#description' => t('Maximal message age in days.'),
-//      '#default_value' => variable_get('message_purge_days', NULL),
-      '#element_validate' => array('element_validate_integer_positive'),
+      '#default_value' => $config->get('purge_quota'),
+//      '#element_validate' => array('element_validate_integer_positive'),
       '#states' => $states,
     );
 
-    $options = array();
-//    foreach (entity_get_info() as $entity_id => $entity) {
-//      $options[$entity_id] = $entity['label'];
-//    }
 
-    $form['message_delete_on_entity_delete'] = array(
+    $options = array();
+    foreach (\Drupal::entityManager()->getDefinitions() as $entity_id => $entity) {
+      if (!$entity->isFieldable()) {
+        continue;
+      }
+
+      $options[$entity_id] = $entity->getLabel();
+    }
+
+    $entities = $config->get('delete_on_entity_delete') ? $config->get('delete_on_entity_delete') : array('node', 'user', 'taxonomy_term', 'comment');
+
+    $form['delete_on_entity_delete'] = array(
       '#title' => t('Auto delete messages referencing the following entities'),
       '#type' => 'select',
       '#multiple' => TRUE,
       '#options' => $options,
-//      '#default_value' => variable_get('message_delete_on_entity_delete', array('node', 'user', 'taxonomy_term', 'comment')),
+      '#default_value' => $entities,
       '#description' => t('Messages that reference entities of these types will be deleted when the referenced entity gets deleted.'),
     );
 
@@ -87,9 +99,13 @@ class MessageSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, array &$form_state) {
-    $this->config('message.message')
-      ->set('file_temporary_path', $form_state['values']['file_private_path'])
-      ->save();
+    $config = $this->config('message.message');
+
+    foreach ($this->defaultKeys() as $key) {
+      $config->set($key, $form_state['values'][$key]);
+    }
+
+    $config->save();
 
     parent::submitForm($form, $form_state);
   }
