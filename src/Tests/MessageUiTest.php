65af9f53294cbe02e572599ac8170e50f1cb14c0
Roy Segall <roy.segall@gizra.com>
2014-07-05 11:07:25 +0300

    Adding to the test the message delete via UI.

--- src/Tests/MessageUiTest.php
+++ src/Tests/MessageUiTest.php
@@ -113,7 +113,10 @@ class MessageUiTest extends MessageTestBase {
     $this->assertTrue($message->getText('he') == 'This is a dummy message with translated text to Hebrew', 'The text in hebrew pulled correctly.');
     $this->assertTrue($message->getText('en') == 'This is a dummy message with some edited dummy text', 'The text in english pulled correctly.');
 
-    // todo: Delete message via the UI.
+    // Delete message via the UI.
+    $this->drupalPostForm('admin/structure/message/delete/dummy_message', array(), 'Delete');
+    $this->assertText(t('There is no Message type yet.'));
+    $this->assertFalse(MessageController::MessageTypeLoad('dummy_message'), 'The message deleted via the UI successfully.');
   }
 
   /**
