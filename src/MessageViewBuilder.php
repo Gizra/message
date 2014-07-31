740d4ce544e4b98736945ec4633bf5f84b32f038
Roy Segall <roy.segall@gizra.com>
2014-07-31 10:07:18 +0300

    Get the partial the user selected for the current view mode.

--- src/MessageViewBuilder.php
+++ src/MessageViewBuilder.php
@@ -19,7 +19,17 @@ class MessageViewBuilder extends EntityViewBuilder {
   public function view(EntityInterface $entity, $view_mode = 'full', $langcode = NULL) {
     $build = parent::view($entity, $view_mode, $langcode);
 
-    // todo: Get the partial that been set in the current view mode.
+    // Load the partials in the correct language.
+    $partials = $entity->getType()->getText(NULL, array('text' => TRUE));
+
+
+    // Get the partials the user selected for the current view mode.
+    $extra_fields = entity_get_display('message', $entity->bundle(), $view_mode);
+    foreach (array_keys($extra_fields->getComponents()) as $partials) {
+      list(, $delta) = explode('_', $partials);
+    }
+
+    // todo: Add this to the rendered array.
     return render($build);
   }
 }
