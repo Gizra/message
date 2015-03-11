0652d963765cd25d561a5e388d18a6d1176c6297
Christian Lopez Espinola <penyaskito@computer.org>
2015-03-11 15:46:19 +0100

    Drupal\Core\Datetime\Date has been renamed to DateFormatter (https://www.drupal.org/node/2309413)

--- src/MessageListBuilder.php
+++ src/MessageListBuilder.php
@@ -7,7 +7,7 @@
 
 namespace Drupal\message;
 
-use Drupal\Core\Datetime\Date;
+use Drupal\Core\Datetime\DateFormatter;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityListBuilder;
 use Drupal\Core\Entity\EntityStorageInterface;
@@ -25,7 +25,7 @@ class MessageListBuilder extends EntityListBuilder {
   /**
    * The date service.
    *
-   * @var \Drupal\Core\Datetime\Date
+   * @var \Drupal\Core\Datetime\DateFormatter
    */
   protected $dateService;
 
@@ -36,10 +36,10 @@ class MessageListBuilder extends EntityListBuilder {
    *   The entity type definition.
    * @param \Drupal\Core\Entity\EntityStorageInterface $storage
    *   The entity storage class.
-   * @param \Drupal\Core\Datetime\Date $date_service
+   * @param \Drupal\Core\Datetime\DateFormatter $date_service
    *   The date service.
    */
-  public function __construct(EntityTypeInterface $entity_type, EntityStorageInterface $storage, Date $date_service) {
+  public function __construct(EntityTypeInterface $entity_type, EntityStorageInterface $storage, DateFormatter $date_service) {
     parent::__construct($entity_type, $storage);
 
     $this->dateService = $date_service;
@@ -52,7 +52,7 @@ class MessageListBuilder extends EntityListBuilder {
     return new static(
       $entity_type,
       $container->get('entity.manager')->getStorage($entity_type->id()),
-      $container->get('date')
+      $container->get('date.formatter')
     );
   }
 
