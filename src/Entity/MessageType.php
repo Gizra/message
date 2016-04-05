edd50c6ad6b093f7dce602ff91e495c32e7295ab
Nir Galon <nirgn975@gmail.com>
2016-04-05 14:23:43 +0300

    Cleanup, and fix checkbox.

--- src/Entity/MessageType.php
+++ src/Entity/MessageType.php
@@ -181,9 +181,7 @@ class MessageType extends ConfigEntityBundleBase implements MessageTypeInterface
    * {@inheritdoc}
    */
   public function getSettings() {
-    return $this->settings + [
-
-    ];
+    return $this->settings;
   }
 
   /**
