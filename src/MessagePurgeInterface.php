c03cf8719c4a1354cbba46028eb207119ec531e8
Amitaibu <amitai@gizra.com>
2016-04-17 13:50:33 +0300

    More boilerplate

--- src/MessagePurgeInterface.php
+++ src/MessagePurgeInterface.php
@@ -20,20 +20,25 @@ use Drupal\Core\Form\FormStateInterface;
  */
 interface MessagePurgeInterface {
 
-  /**
-   * Registers a soon to be deleted group entity, for processing.
-   *
-   * During processing its orphaned members or content will be deleted.
-   *
-   * @param \Drupal\Core\Entity\EntityInterface $entity
-   *   The group entity to register.
-   */
-  public function register(EntityInterface $entity);
 
   /**
-   * Starts the deletion process.
+   * Fetch the messages that need to be purged.
    */
-  public function process();
+  public function fetch();
+
+
+  /**
+   * Process the purgeable messages.
+   *
+   * Normally this is a bulk delete operation.
+   *
+   * @param array $ids
+   *   The message IDs to be processed.
+   *
+   * @return bool
+   *   The result of the process.
+   */
+  public function process(array $ids);
 
   /**
    * Returns the configuration form elements specific to this plugin.
