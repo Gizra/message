38075b2c89228d229b8ceade3facdb7bd9bc7684
Amitaibu <amitai@gizra.com>
2016-05-16 14:00:42 +0300

    Better use config settings

--- src/Plugin/MessagePurge/Days.php
+++ src/Plugin/MessagePurge/Days.php
@@ -20,13 +20,24 @@ class Days extends MessagePurgeBase {
   /**
    * {@inheritdoc}
    */
-  public function configurationForm($form, FormStateInterface $form_state) {
-    return [
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form['days'] = [
       '#type' => 'textfield',
       '#title' => t('Messages older than'),
       '#description' => t('Maximal message age in days.'),
       '#default_value' => $this->configFactory->get('message.settings')->get('purge_quota'),
     ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    parent::submitConfigurationForm($form, $form_state);
+
+    $this->configuration['days'] = $form_state->getValue('days');
   }
 
   /**
