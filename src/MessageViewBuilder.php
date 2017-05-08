b1b30c588472aa9bca5827d8621ee9ac0156d47a
Sami Hellsten <sami.hellsten@mediamaisteri.com>
2017-05-08 18:45:33 +0300

    Simplify langcode checking

--- src/MessageViewBuilder.php
+++ src/MessageViewBuilder.php
@@ -16,19 +16,8 @@ class MessageViewBuilder extends EntityViewBuilder {
   public function view(EntityInterface $entity, $view_mode = 'full', $langcode = NULL) {
     $build = parent::view($entity, $view_mode, $langcode);
 
-    // Ensure that wanted language exists for given message. If no langcode
-    // given, check if users current language exists for message.
-    $current_language = \Drupal::languageManager()->getCurrentLanguage()->getId();
     /* @var \Drupal\message\Entity\Message $entity  */
-    if ($langcode && $entity->getText($langcode)) {
-      $entity->setLanguage($langcode);
-    }
-    elseif ($entity->getText($current_language)) {
-      $entity->setLanguage($current_language);
-    }
-
-    // Load the partials in the correct language.
-    $partials = $entity->getText();
+    $partials = $entity->getText($langcode);
 
     // Get the partials the user selected for the current view mode.
     $extra_fields = entity_get_display('message', $entity->bundle(), $view_mode);
