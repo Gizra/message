31e1e32d683c1c9d5a8305a0cffe6cbd1b8e1bdc
Marco Wolf <marcowolf1987@googlemail.com>
2017-11-13 12:45:43 +0100

    Remove because it is only needed in MessageTemplate.

--- src/MessageInterface.php
+++ src/MessageInterface.php
@@ -95,14 +95,6 @@ interface MessageInterface extends ContentEntityInterface, EntityOwnerInterface 
   public function setLanguage($language);
 
   /**
-   * Return the text.
-   *
-   * @return array
-   *   The message text.
-   */
-  public function getRawText();
-  
-  /**
    * Replace arguments with their placeholders.
    *
    * @param string $langcode
