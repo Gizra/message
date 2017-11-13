db666c75b6026fd96a65c23f4311ae82dc558930
Marco Wolf <marcowolf1987@googlemail.com>
2017-11-13 12:19:01 +0100

    Add MessageTemplate process plugin for the text.

---
+++ src/Plugin/migrate/process/MessageTemplateProcessText.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\message\Plugin\migrate\process;
+
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\Row;
+
+/**
+ * @MigrateProcessPlugin(
+ *   id = "d7_message_template_text"
+ * )
+ */
+class MessageTemplateProcessText extends ProcessPluginBase {
+
+  /**
+   * {@inheritdoc}
+   *
+   * Transform the message template texts.
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    $source = $row->getSource();
+    $message_tpl = \Drupal::entityManager()->getStorage('message_template')->load($row->getSource()['name']);
+
+    $texts = [];
+    if (!empty($message_tpl)) {
+      $texts = $message_tpl->getRawText();
+    }
+
+    $texts[] = [
+      'value' => $source['message_text_value'],
+      'format' => $source['message_text_format'],
+    ];
+
+    return $texts;
+  }
+
+}
