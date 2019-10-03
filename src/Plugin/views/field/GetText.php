b06678165213a0edb97d0deb309a24ba249b084e
adan <adan@516420.no-reply.drupal.org>
2019-10-03 14:21:28 -0700

    Issue #3012633 by manuel.adan: Add delta option to the text message field in views

--- src/Plugin/views/field/GetText.php
+++ src/Plugin/views/field/GetText.php
@@ -3,6 +3,7 @@
 namespace Drupal\message\Plugin\views\field;
 
 use Drupal\Component\Render\FormattableMarkup;
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\views\Plugin\views\field\FieldPluginBase;
 use Drupal\views\ResultRow;
 
@@ -31,7 +32,31 @@ class GetText extends FieldPluginBase {
    * {@inheritdoc}
    */
   public function render(ResultRow $values) {
-    return new FormattableMarkup(implode($values->_entity->getText(), "\n"), []);
+    $delta = is_numeric($this->options['delta']) ? $this->options['delta'] : NULL;
+    return new FormattableMarkup(implode($values->_entity->getText(NULL, $delta), "\n"), []);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function defineOptions() {
+    $options = parent::defineOptions();
+    $options['delta'] = ['default' => ''];
+    return $options;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildOptionsForm(&$form, FormStateInterface $form_state) {
+    $form['delta'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Delta'),
+      '#size' => 5,
+      '#default_value' => $this->options['delta'],
+    ];
+
+    parent::buildOptionsForm($form, $form_state);
   }
 
 }
